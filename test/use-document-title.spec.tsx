3efcf5ca9a967194428a11aff1d7ef388fa71d47
Bartosz Gordon <bartosz.gordon@sealcode.org>
2019-06-22 10:05:36 +0200

    Test useDocumentTitle hook

---
+++ test/use-document-title.spec.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import * as React from 'react';
+
+import { useDocumentTitle } from '../src';
+
+function App() {
+  useDocumentTitle('tools');
+  return <div />;
+}
+
+describe('useDocumentTitle', () => {
+  it('should set a document title', () => {
+    render(<App />);
+    expect(window.document.title).toEqual('tools');
+  });
+});
